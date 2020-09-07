e7e2da59e207ed5620b93fd1d079a33ebe8dcaa0
ryanking13 <def6488@gmail.com>
2020-09-07 17:49:21 +0900

    [WIP] Fix dataset loader to fixed output label

--- tf_version/dataset.py
+++ tf_version/dataset.py
@@ -13,16 +13,111 @@ import settings
 
 
 class KCaptchaDataLoader:
-    def __init__(self):
-        self.train_df = load_trainset()
-        self.test_df = load_testset()
-        self.validation_df = load_validationset()
+    def __init__(
+        self,
+        trainset_path=settings.TRAIN_DATASET_PATH,
+        testset_path=settings.TEST_DATASET_PATH,
+        validationset_path=settings.VALIDATION_DATASET_PATH,
+    ):
+        self.trainset_path = trainset_path
+        self.testset_path = testset_path
+        self.validationset_path = validationset_path
+        self.separator = "_"
+        self.ext = ".png"
+
+        self.datagen = image.ImageDataGenerator(
+            rescale=1.0 / 255,
+            preprocessing_function=mobilenet_v2.preprocess_input,
+        )
+        self.dataset_loaded = False
+
+        self.x_train = None
+        self.y_train = None
+        self.x_val = None
+        self.y_val = None
+        self.x_test = None
+        self.y_test = None
+
+        self.train_df = self.dataset_files_to_df(trainset_path)
+        self.test_df = self.dataset_files_to_df(testset_path)
+        self.trainset_generator, self.trainset_size = self.df_to_generator(
+            self.train_df
+        )
+        self.testset_generator, self.testset_size = self.df_to_generator(self.test_df)
+
+    def one_hot_encode(self, label):
+        vector = np.zeros(settings.CHAR_SET_LEN * settings.CAPTCHA_LENGTH, dtype=float)
+        for i, c in enumerate(label):
+            idx = i * settings.CHAR_SET_LEN + int(c)
+            vector[idx] = 1.0
+        return vector
+
+    def one_hot_decode(self, vec):
+        char_pos = vec.nonzero()[0]
+        text = []
+        for i, c in enumerate(char_pos):
+            digit = c % settings.CHAR_SET_LEN
+            text.append(str(digit))
+        return "".join(text)
+
+    def load_dataset(self):
+        def _load_dataset_from_dir(path):
+            dataset_dir = pathlib.Path(path)
+            files = dataset_dir.glob(f"*.{self.ext}")
+
+            x, y = [], []
+            for f in files:
+                filepath = f.as_posix()
+                img = self.preprocess(filepath)
+                x.append(img)
+                label = f.name.split(self.separator)[0]
+                y.append(self.one_hot_encode(label))
+
+            return x, y
+
+        self.x_train, self.y_train = _load_dataset_from_dir(self.trainset_path)
+        self.x_val, self.y_val = _load_dataset_from_dir(self.validationset_path)
+        self.x_test, self.y_test = _load_dataset_from_dir(self.testset_path)
+
+    def get_trainset(self, batch_size=64):
+        return (
+            self.datagen.flow(
+                x=self.x_train,
+                y=self.y_train,
+                batch_size=batch_size,
+                shuffle=True,
+            ),
+            len(self.y_train),
+        )
+
+    def dataset_files_to_df(self, path, ext=".png", separator="_"):
+        """generate Pandas DataFrame for dataset generator"""
+
+        dataset_dir = pathlib.Path(path)
+        files = dataset_dir.glob(f"*.{ext}")
+
+        labels = []
+        filepaths = []
+        for f in files:
+            filepaths.append(f.as_posix())
+            label = f.name.split(separator)[0]
+            labels.append(self.one_hot_encode(label))
+
+        df = pd.Dataframe(
+            {
+                "file": filepaths,
+                "label": labels,
+            }
+        )
+        df = df.dropna()
+
+        return df
 
     def preprocess(self, img_path):
-        img = tf.keras.preprocessing.image.load_img(
+        img = image.load_img(
             img_path, target_size=(settings.IMAGE_WIDTH, settings.IMAGE_HEIGHT)
         )
-        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = image.img_to_array(img)
         return img
 
     def load_dataset(self, dataset, batch_size=64, subset="training"):
@@ -43,7 +138,13 @@ class KCaptchaDataLoader:
 
         datagen.fit(x)
         return (
-            datagen.flow(x=x, y=y, batch_size=batch_size, shuffle=True, subset=subset,),
+            datagen.flow(
+                x=x,
+                y=y,
+                batch_size=batch_size,
+                shuffle=True,
+                subset=subset,
+            ),
             len(x),
         )
 
@@ -53,38 +154,15 @@ class KCaptchaDataLoader:
         )
 
     def load_testset(self, batch_size=64):
-        return self.load_dataset(self.test_df, batch_size=batch_size,)
+        return self.load_dataset(
+            self.test_df,
+            batch_size=batch_size,
+        )
 
     def load_validationset(self, batch_size=64):
         return self.load_dataset(
             self.train_df, batch_size=batch_size, subset="validation"
         )
-
-
-def parse_dataset(dataset_path, ext="png"):
-    def parse_info_from_file(path, separator="_"):
-        fname = path.name
-        label_str = fname.split(separator)[0]
-        label = [10, 10, 10, 10, 10, 10]
-        for i, s in enumerate(label_str):
-            label[i] = int(s)
-        return tuple(label)
-
-    dataset_dir = pathlib.Path(dataset_path)
-    files = dataset_dir.glob(f"*.{ext}")
-
-    records = []
-    filenames = []
-    for f in files:
-        filenames.append(f.as_posix())
-        info = parse_info_from_file(f)
-        records.append(info)
-
-    df = pd.DataFrame(records)
-    df["file"] = filenames
-    df = df.dropna()
-
-    return df
 
 
 def plot_distribution(pd_series):
@@ -96,15 +174,3 @@ def plot_distribution(pd_series):
     fig.update_layout(title_text="Distribution for %s" % pd_series.name)
 
     fig.show()
-
-
-def load_trainset():
-    return parse_dataset(settings.TRAIN_DATASET_PATH)
-
-
-def load_testset():
-    return parse_dataset(settings.TEST_DATASET_PATH)
-
-
-def load_validationset():
-    return parse_dataset(settings.VALIDATION_DATASET_PATH)
--- tf_version/settings.py
+++ tf_version/settings.py
@@ -1,16 +1,15 @@
-import os
+import pathlib
 
 NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 CHAR_SET = NUMBER
 CHAR_SET_LEN = len(CHAR_SET)
 
-CAPTCHA_LENGTH_MIN = 2
-CAPTCHA_LENGTH_MAX = 6
+CAPTCHA_LENGTH = 2
 
 IMAGE_WIDTH = 160
 IMAGE_HEIGHT = 60
 
-TRAIN_DATASET_PATH = os.path.join("..", "dataset_generator", ".data", "train")
-TEST_DATASET_PATH = os.path.join("..", "dataset_generator", ".data", "test")
-VALIDATION_DATASET_PATH = os.path.join("..", "dataset_generator", ".data", "validation")
+TRAIN_DATASET_PATH = (pathlib.Path() / "../dataset_generator/.data/train").resolve()
+TEST_DATASET_PATH = (pathlib.Path() / "../dataset_generator/.data/test").resolve()
+VALIDATION_DATASET_PATH = (pathlib.Path() / "../dataset_generator/.data/validation").resolve()
 
