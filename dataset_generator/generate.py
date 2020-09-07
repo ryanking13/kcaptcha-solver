d70ad59bcd2a30114a2e3c030c5b090ace3fcab4
ryanking13 <def6488@gmail.com>
2020-09-07 15:51:45 +0900

    Support commandline arguments in dataset generation

--- dataset_generator/generate.py
+++ dataset_generator/generate.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import subprocess as sp
 import random
 import pathlib
@@ -10,14 +11,42 @@ PORT = 9999
 DATASET_DIR = ".data"
 TRAINSET_DIR = "train"
 TESTSET_DIR = "test"
-VALIDATIONSET_DIR = "validation"
 DATASET_SIZE = 50000
-TRAIN_RATIO = 0.6
-VALIDATION_RATIO = 0.2
-TEST_RATIO = 0.2
-TRAINSET_SIZE = int(DATASET_SIZE * TRAIN_RATIO)
-TESTSET_SIZE = int(DATASET_SIZE * TEST_RATIO)
-VALIDATIONSET_SIZE = int(DATASET_SIZE * VALIDATION_RATIO)
+TRAIN_TEST_RATIO = 0.8
+NUM_DIGITS = 2
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--dataset-size",
+        default=DATASET_SIZE,
+        help="Dataset size (Default: %(default)s",
+        type=int,
+    )
+    parser.add_argument(
+        "-d",
+        "--dataset-dir",
+        default=DATASET_DIR,
+        help="Directory where dataset will be saved (Default: %(default)s)",
+    )
+    parser.add_argument(
+        "-r",
+        "--train-test-ratio",
+        default=TRAIN_TEST_RATIO,
+        help="Train/Test set ratio (Default: %(default)s)",
+        type=float,
+    )
+    parser.add_argument(
+        "-n",
+        "--num-digits",
+        default=NUM_DIGITS,
+        help="Number of CAPTCHA digits (Default: %(default)s)",
+        type=int,
+    )
+
+    return parser.parse_args()
 
 
 def run_kcaptcha_server(docroot, port):
@@ -38,51 +67,39 @@ def run_kcaptcha_server(docroot, port):
 #     return str(download_dir / target)
 
 
-def generate_data(count, download_dir, port):
+def generate_data(count, download_dir, port, length):
     nums = "0123456789"
     for i in range(count):
-        length = random.choices([2, 3, 4, 5, 6], weights=[1, 10, 100, 1000, 10000], k=1)[0]
+        # length = random.choices(
+        #     [2, 3, 4, 5, 6], weights=[1, 10, 100, 1000, 10000], k=1
+        # )[0]
         target = "".join(random.choices(nums, k=length))
-        save_path = download_dir / ("%s_%.5d.png" % (target, i))
+        save_path = download_dir / ("%s_%.6d.png" % (target, i))
         urllib.request.urlretrieve(
-            "http://localhost:%d?string=%s" % (port, target), filename=save_path,
+            "http://localhost:%d?string=%s" % (port, target),
+            filename=save_path,
         )
 
     return str(download_dir)
 
 
 def main():
+    args = parse_args()
     proc = run_kcaptcha_server(KCAPTCHA_DIR, PORT)
 
-    dataset_dir = pathlib.Path(DATASET_DIR)
+    dataset_dir = pathlib.Path(args.dataset_dir)
     trainset_dir = dataset_dir / TRAINSET_DIR
     testset_dir = dataset_dir / TESTSET_DIR
-    validationset_dir = dataset_dir / VALIDATIONSET_DIR
-    if not dataset_dir.is_dir():
-        dataset_dir.mkdir()
-        trainset_dir.mkdir()
-        testset_dir.mkdir()
-        validationset_dir.mkdir()
+    dataset_dir.is_dir() or dataset_dir.mkdir()
+    trainset_dir.is_dir() or trainset_dir.mkdir()
+    testset_dir.is_dir() or testset_dir.mkdir()
 
-    # targets = ["%.2d" % num for num in range(0, 100)]
-    # pool = concurrent.futures.ThreadPoolExecutor()
-    # futures = []
-    # for target in targets:
-    #     pool.submit(
-    #         generate_data, target, TRAINSET_SIZE // len(targets), trainset_dir, PORT
-    #     )
-    #     pool.submit(
-    #         generate_data, target, TESTSET_SIZE // len(targets), testset_dir, PORT
-    #     )
-    #     pool.submit(
-    #         generate_data, target, VALIDATIONSET_SIZE // len(targets), validationset_dir, PORT
-    #     )
-
+    trainset_size = int(args.dataset_size * args.train_test_ratio)
+    testset_size = args.dataset_size - trainset_size
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [
-            executor.submit(generate_data, TRAINSET_SIZE, trainset_dir, PORT),
-            executor.submit(generate_data, TESTSET_SIZE, testset_dir, PORT),
-            executor.submit(generate_data, VALIDATIONSET_SIZE, validationset_dir, PORT),
+            executor.submit(generate_data, trainset_size, trainset_dir, PORT, args.num_digits),
+            executor.submit(generate_data, testset_size, testset_dir, PORT, args.num_digits),
         ]
 
         try:
