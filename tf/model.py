e274a253df230dd0ad7076f634a7a11ed5049499
ryanking13 <def6488@gmail.com>
2020-12-02 21:34:53 +0900

    Add accuracy metric

--- tf/model.py
+++ tf/model.py
@@ -2,12 +2,13 @@ import datetime
 
 import tensorflow as tf
 from tensorflow.keras.applications import mobilenet_v2
-
+from tensorflow.keras.applications import DenseNet121
 # from tensorflow.keras.applications import mobilenet
 from tensorflow.keras import layers
 from tensorflow.keras import activations
 from tensorflow.keras import models
 from tensorflow.keras import callbacks
+from tensorflow.keras import metrics
 import numpy as np
 
 import settings
@@ -20,31 +21,43 @@ class CAPTCHAMobileNet:
         input_tensor=layers.Input(shape=(224, 224, 3)),
         length=1000,
     ):
-        self.mobilenet = mobilenet_v2.MobileNetV2(
+        # self.net = mobilenet_v2.MobileNetV2(
+        #     # self.mobilenet = mobilenet.MobileNet(
+        #     input_shape=input_shape,
+        #     input_tensor=input_tensor,
+        #     alpha=1.0,
+        #     include_top=False,
+        #     # weights="imagenet",
+        #     weights=None,
+        #     pooling="max",
+        # )
+
+        self.net = DenseNet121(
             # self.mobilenet = mobilenet.MobileNet(
             input_shape=input_shape,
             input_tensor=input_tensor,
-            alpha=1.0,
             include_top=False,
             weights="imagenet",
-            # weights=None,
             pooling="max",
         )
 
-        # for layer in self.mobilenet.layers:
+        # for layer in self.net.layers:
         #     layer.trainable = False
 
-        fc1 = layers.Dense(1024)(self.mobilenet.output)
+        fc1 = layers.Dense(1024)(self.net.output)
         fc2 = layers.Dense(length)(fc1)
 
         prediction = activations.sigmoid(fc2)
 
         self.model = models.Model(inputs=input_tensor, outputs=prediction)
         opt = tf.keras.optimizers.Adam(learning_rate=1e-4)
+        def top_k_accuracy(y_true, y_pred):
+            return metrics.top_k_categorical_accuracy(y_true, y_pred, k=settings.CAPTCHA_LENGTH)
+
         self.model.compile(
             optimizer=opt,
             loss="binary_crossentropy",
-            metrics=["accuracy"],
+            metrics=["accuracy", top_k_accuracy],
         )
 
         # self.model.summary()
